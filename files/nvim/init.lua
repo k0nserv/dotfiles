cfe1eddcbc690d00b8cbe6dbfc555acded462a6e
Hugo Tunius <h@tunius.se>
2023-07-31 09:42:53 +0100

    Change format on save auto command

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -546,12 +546,12 @@ nvim_lsp.efm.setup {
     debounce_text_changes = 150,
   },
 }
-vim.cmd([[autocmd! BufWritePre *.ts :lua vim.lsp.buf.formatting_seq_sync()]])
-vim.cmd([[autocmd! BufWritePre *.tsx :lua vim.lsp.buf.formatting_seq_sync()]])
-vim.cmd([[autocmd! BufWritePre *.js :lua vim.lsp.buf.formatting_seq_sync()]])
-vim.cmd([[autocmd! BufWritePre *.css :lua vim.lsp.buf.formatting_seq_sync()]])
-vim.cmd([[autocmd! BufWritePre *.json :lua vim.lsp.buf.formatting_seq_sync()]])
-vim.cmd([[autocmd! BufWritePre *.py :lua vim.lsp.buf.formatting_seq_sync()]])
+vim.cmd([[autocmd! BufWritePre *.ts :lua vim.lsp.buf.format()]])
+vim.cmd([[autocmd! BufWritePre *.tsx :lua vim.lsp.buf.format()]])
+vim.cmd([[autocmd! BufWritePre *.js :lua vim.lsp.buf.format()]])
+vim.cmd([[autocmd! BufWritePre *.css :lua vim.lsp.buf.format()]])
+vim.cmd([[autocmd! BufWritePre *.json :lua vim.lsp.buf.format()]])
+vim.cmd([[autocmd! BufWritePre *.py :lua vim.lsp.buf.format()]])
 
 ------------------------------------------
 -- Key maps
